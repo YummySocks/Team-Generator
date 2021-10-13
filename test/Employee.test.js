e4397d0331678fba357c1f5ba1ee352f3201d640
Socks <patricksutcliffe123@gmail.com>
2021-10-12 22:21:18 -0400

    adding tests

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,1 +1,31 @@
 const Employee = require('../lib/Employee')
+
+describe("Employee",() =>{
+    describe("name",()=> {
+        it("should return the name given", () =>{
+            const empTest = new Employee("patrick","3","what@gmail.com")
+
+            const nameTest = "patrick"
+
+            expect(nameTest).toEqual(empTest.getName())
+        })
+    })
+    describe("id",()=> {
+        it("should return the id given", () =>{
+            const empTest = new Employee("patrick","3","what@gmail.com")
+
+            const idTest = "3"
+
+            expect(idTest).toEqual(empTest.getId())
+        })
+    })
+    describe("name",()=> {
+        it("should return the name given", () =>{
+            const empTest = new Employee("patrick","3","what@gmail.com")
+
+            const emailTest = "what@gmail.com"
+
+            expect(emailTest).toEqual(empTest.getEmail())
+        })
+    })
+})
--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,1 +1,40 @@
 const Engineer = require('../lib/Engineer')
+
+describe("Engineer",() =>{
+    describe("name",()=> {
+        it("should return the name given", () =>{
+            const engTest = new Engineer("patrick","3","what@gmail.com","Yummily")
+
+            const nameTest = "patrick"
+
+            expect(nameTest).toEqual(engTest.getName())
+        })
+    })
+    describe("id",()=> {
+        it("should return the id given", () =>{
+            const engTest = new Engineer("patrick","3","what@gmail.com","Yummily")
+
+            const idTest = "3"
+
+            expect(idTest).toEqual(engTest.getId())
+        })
+    })
+    describe("name",()=> {
+        it("should return the name given", () =>{
+            const engTest = new Engineer("patrick","3","what@gmail.com","Yummily")
+
+            const emailTest = "what@gmail.com"
+
+            expect(emailTest).toEqual(engTest.getEmail())
+        })
+    })
+    describe("github",()=> {
+        it("should return the github given", () =>{
+            const engTest = new Engineer("patrick","3","what@gmail.com","Yummily")
+
+            const gitTest = "Yummily"
+
+            expect(gitTest).toEqual(engTest.getGithub())
+        })
+    })
+})
